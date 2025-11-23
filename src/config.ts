853417e4a00ce81c8b36d2db9c4c85a6bbb21f84
westme1anon <we57me1on@gmail.com>
2025-11-23 14:51:52 +0800

    feat: 添加Astro部署工作流

--- src/config.ts
+++ src/config.ts
@@ -22,7 +22,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "wes1",
 	subtitle: "ctfer",
-	siteURL: "https://wes1.cn/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://www.wes1.cn/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-11-07", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
