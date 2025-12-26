e140548f7c54112d3db6d2366379dba7ed4ab276
westme1anon <we57me1on@gmail.com>
2025-12-26 19:18:16 +0800

    12-26更新顶栏头像

--- src/config.ts
+++ src/config.ts
@@ -51,7 +51,7 @@ export const siteConfig: SiteConfig = {
 		// 顶栏标题文本
 		text: "",
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.png
-		icon: "assets/home/home.png",
+		icon: "assets/home/circlekumiko0.png",
 	},
 
 	bangumi: {
